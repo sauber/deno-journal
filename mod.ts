31323a9ea02f17a467120683f61c8c9f918288be
Søren Døssing <netcom@sauber.net>
2025-06-25 14:10:43 +0200

    Move mod.ts to root folder

---
+++ mod.ts
@@ -0,0 +1,8 @@
+export { JournaledAsset } from "./src/journaled-asset.ts";
+export { Asset } from "./src/asset.ts";
+export { Backend } from "./src/backend.ts";
+export { DiskBackend } from "./src/disk-backend.ts";
+export { TempBackend } from "./src/temp-backend.ts";
+export { HeapBackend } from "./src/heap-backend.ts";
+export { CachingBackend } from "./src/caching-backend.ts";
+export type { JSONValue, JSONObject, AssetName, AssetNames } from "./src/types.ts";
